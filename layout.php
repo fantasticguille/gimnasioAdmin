74bea9f535790b27a2e860339fb51b20e31d18b1
unknown <guillac124@gmail.com>
2015-09-24 20:08:10 -0300

    h4 con codigo php

---
+++ layout.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Maquetando en html5</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="layout.css">
+</head>
+<body>
+
+<header>
+<h1>City Gallery</h1>
+<h4><?php date(); ?></h4>
+</header>
+
+<nav>
+London<br>
+Paris<br>
+Tokyo<br>
+</nav>
+
+<section>
+<h1>London</h1>
+<p>
+London is the capital city of England. It is the most populous city in the United Kingdom,
+with a metropolitan area of over 13 million inhabitants.
+</p>
+<p>
+Standing on the River Thames, London has been a major settlement for two millennia,
+its history going back to its founding by the Romans, who named it Londinium.
+</p>
+</section>
+
+<section>
+<h2>London</h2>
+<p>
+London is the capital city of England. It is the most populous city in the United Kingdom,
+with a metropolitan area of over 13 million inhabitants.
+</p>
+<h3>Otro Título</h3>
+<p>
+Standing on the River Thames, London has been a major settlement for two millennia,
+its history going back to its founding by the Romans, who named it Londinium.
+</p>
+</section>
+
+<footer>
+Copyright © W3Schools.com
+</footer>
+
+</body>
+</html>
